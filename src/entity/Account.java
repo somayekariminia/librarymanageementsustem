6d8fb429b9196a07bc0150b42f72b0ca400df209
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-15 15:17:17 +0430

    update Account

--- src/entity/Account.java
+++ src/entity/Account.java
@@ -3,7 +3,6 @@ package entity;
 import java.util.List;
 
 public class Account {
-    private User user;
     private String userName;
     private String password;
     private List<Book> no_borrowed_books;
@@ -11,4 +10,19 @@ public class Account {
     private List<Book> no_returned_books;
     private List<Book> no_lost_books_fine_amount;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
