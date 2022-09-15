79aa9e0b0f617b8fb9295022794e09846a8d0704
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-15 11:06:23 +0430

    add Account class

--- src/entity/Account.java
+++ src/entity/Account.java
@@ -1,4 +1,8 @@
 package entity;
 
 public class Account {
+   private int no_borrowed_books;
+   private int no_reserved_books;
+   private int no_returned_books;
+   private int no_lost_books_fine_amount;
 }
