32f3cead23d2ef0c78ef7693628c84a147ba49f3
somaye6868 <somayekariminia6868@gmail.com>
2022-09-15 16:14:31 +0430

    add update mrthod

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -1,9 +1,12 @@
 package entity;
 
+import java.sql.Date;
+
 public class Book {
     private String title;
     private String author;
     private String ISBN;
     private String publication;
-
+    private boolean reserv;
+    private Date duedt;
 }
--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -1,4 +1,22 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BookRepository {
+
+   public void update(Date duedt,boolean reserv,String name ) throws SQLException {
+       Connection connection=MyConnection.getConnection();
+       PreparedStatement preparedStatement=connection.prepareStatement("update book_tbl set duedt=?,reserv=? where name=?");
+       preparedStatement.setDate(1,duedt);
+       preparedStatement.setBoolean(2,reserv);
+       preparedStatement.setString(3,name);
+       preparedStatement.executeUpdate();
+   }
+   public boolean isReserve(){
+       Connection connection=MyConnection.getConnection();
+       PreparedStatement preparedStatement=connection.prepareStatement()
+   }
 }
