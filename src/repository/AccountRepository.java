8bc8a9ee81d3dabe252651dcb7e2ca75e6e2387d
fahimeyarmohammadi <fahimeyarmohammadi619@gmail.com>
2022-09-15 15:22:52 +0430

    add BookRepository class

--- src/repository/AccountRepository.java
+++ src/repository/AccountRepository.java
@@ -1,8 +1,44 @@
 package repository;
 
+import entity.Account;
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AccountRepository {
 
-    public void logIn(){
+    public int logIn(String username, String password) throws SQLException {
+        Connection connection=MyConnection.getConnection();
+        PreparedStatement preparedStatement= connection.prepareStatement("select id from account_tbl" +
+                " where userName=? , password=? ");
+        preparedStatement.setString(1,username);
+        preparedStatement.setString(2,password);
 
+        ResultSet resultSet=preparedStatement.executeQuery();
+        int id=0;
+        while(resultSet.next()){
+             id=resultSet.getInt("id");
+        }
+        return id;
+    }
+
+    public void register(Account account) throws SQLException {
+        Connection connection=MyConnection.getConnection();
+        PreparedStatement preparedStatement= connection.prepareStatement("insert into account_tbl" +
+                " (userName,password) values (?,?)");
+        preparedStatement.setString(1,account.getUserName());
+        preparedStatement.setString(2,account.getPassword());
+        preparedStatement.executeUpdate();
+    }
+    public void deleteAccount(Account account ) throws SQLException {
+        Connection connection=MyConnection.getConnection();
+        PreparedStatement preparedStatement= connection.prepareStatement("delete account+tbl " +
+                "where userName=? and password=?");
+        preparedStatement.setString(1, account.getUserName());
+        preparedStatement.setString(2, account.getPassword());
+        preparedStatement.executeUpdate();
     }
 }
--- src/repository/MyConnection.java
+++ src/repository/MyConnection.java
@@ -1,4 +1,12 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyConnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/maktab", "postgres", "Fy4965285");
+
+    }
 }
